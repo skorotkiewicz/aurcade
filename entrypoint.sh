9f8c5201f00b1c1bb45f13b6c358f70141a2ba53
Sebastian Korotkiewicz <skorotkiewicz@gmail.com>
2026-08-09 12:16:48 +0200

    Add persistent SSH host keys support
    Mount ssh_host_keys volume and generate keys on first run
    instead of using default ssh-keygen -A
    </div></tool_call>

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,8 @@ export AUR_REPOS_ROOT
 
 aur-repos setup
 chown -R git:git "$AUR_REPOS_ROOT" /home/git/.ssh
-ssh-keygen -A
-/usr/sbin/sshd
+host_key=/etc/ssh/host_keys/ssh_host_ed25519_key
+install -d -m 700 /etc/ssh/host_keys
+[ -f "$host_key" ] || ssh-keygen -q -t ed25519 -N '' -f "$host_key"
+/usr/sbin/sshd -h "$host_key"
 exec lighttpd -D -f /etc/lighttpd/lighttpd.conf
