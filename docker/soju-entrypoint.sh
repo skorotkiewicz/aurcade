0287057e0d0bbc749a78297b270b3b5e723c430f
Sebastian Korotkiewicz <skorotkiewicz@gmail.com>
2026-08-10 15:11:24 +0200

    Remove aurcade health check and use insecure network
    - Removed aurcade depends_on health check in compose.yml
    - Updated network address to insecure protocol in entrypoint script
    - Added /run/soju/admin cleanup in entrypoint

--- docker/soju-entrypoint.sh
+++ docker/soju-entrypoint.sh
@@ -10,6 +10,7 @@ install -d -m 700 -o soju -g soju /run/soju /soju-data /soju-data/tls
 install -m 644 -o soju -g soju /etc/aurcade/services/soju-fullchain.pem /soju-data/tls/fullchain.pem
 install -m 600 -o soju -g soju /etc/aurcade/services/soju-privkey.pem /soju-data/tls/privkey.pem
 chown -R soju:soju /soju-data
+rm -f /run/soju/admin
 su-exec soju soju -config "$config" &
 pid=$!
 trap 'kill "$pid" 2>/dev/null || true' INT TERM EXIT
@@ -19,9 +20,9 @@ until [ -S /run/soju/admin ]; do
 done
 while IFS="	" read -r account admin network; do
     ctl="su-exec soju sojuctl -config $config"
-    $ctl user create -username "$account" -disable-password -admin "$admin" >/dev/null 2>&1 \
-        || $ctl user update "$account" -disable-password -admin "$admin" >/dev/null
-    $ctl user run "$account" network create -addr irc://ergo:6667 -name "$network" -nick "$account" >/dev/null 2>&1 \
-        || $ctl user run "$account" network update "$network" -addr irc://ergo:6667 -nick "$account" >/dev/null
+    $ctl user create -username "$account" -disable-password -admin="$admin" >/dev/null 2>&1 \
+        || $ctl user update "$account" -disable-password -admin="$admin" >/dev/null
+    $ctl user run "$account" network create -addr irc+insecure://ergo:6667 -name "$network" -nick "$account" >/dev/null 2>&1 \
+        || $ctl user run "$account" network update "$network" -addr irc+insecure://ergo:6667 -nick "$account" >/dev/null
  done < "$users"
 wait "$pid"
